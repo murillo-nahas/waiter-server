400bf080e31e0592f43a889e4fd9df1c8f41ad89
murillo.nahas <murillonahas@hotmail.com>
2023-06-26 21:27:10 -0300

    feat: product  model

---
+++ src/app/models/Product.ts
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+import { model, Schema } from "mongoose";
+
+export const Product = model(
+  "Product",
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imagePath: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      required: true,
+      type: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          icon: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Category",
+    },
+  })
+);

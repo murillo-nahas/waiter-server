c0981816d3f5bb9d043234392a8a78d28e3b5d31
murillo.nahas <murillonahas@hotmail.com>
2023-06-26 21:27:02 -0300

    feat: order model

---
+++ src/app/models/Order.ts
@@ -0,0 +1,39 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+import { model, Schema } from "mongoose";
+
+export const Order = model(
+  "Order",
+  new Schema({
+    table: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["WAITING", "IN_PRODUCTION", "DONE"],
+      default: "WAITING",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+    products: {
+      required: true,
+      type: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "Product",
+          },
+          quantity: {
+            type: Number,
+            default: 1,
+          },
+        },
+      ],
+    },
+  })
+);
